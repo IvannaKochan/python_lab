f6c910e97fc5398832b36d42005095ee528d9dfb
5095 <ivanna.kochan.ir.2022@lpnu.ua>
2023-05-23 00:41:28 +0300

    change main lab_8

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
+"""
+Main method
+"""
+# pylint: disable=import-error
 from lab_8.managers.manager import GameManager
 from lab_8.models.educational_game import EducationalGame
 from lab_8.models.mobile_game import MobileGame
-from models.board_game import BoardGame
-from models.computer_game import ComputerGame
+from lab_8.models.board_game import BoardGame
+from lab_8.models.computer_game import ComputerGame
 
 game_list = GameManager()
 
 for _ in range(2):
     game_list.add_game(BoardGame("Dixid", "First Company", 2018, 3, 12, 12))
     game_list.add_game(ComputerGame("S.T.A.L.K.E.R. 2", "GSC Game World", 2023, 2, 100, 1))
-    game_list.add_game(MobileGame("Clash Of Clans","Publisher 2", 2012, 2, 2, 2))
+    game_list.add_game(MobileGame("Clash Of Clans", "Publisher 2", 2012, 2, 2, 2))
     game_list.add_game(EducationalGame("AR Book", "Publisher 3", 2023, 1, 1, 1))
 
 for game in game_list.games:
-    print(game)
+    print(game.to_string())
 
 print("\n")
 
-list_games_yanger_than = game_list.find_games_younger_than(2021)
-for _ in list_games_yanger_than:
-    print(_)
+list_games_younger_than = game_list.find_games_younger_than(2021)
+for element in list_games_younger_than:
+    print(element)
 
 print("\n")
 
 list_games_for_needed_player = game_list.find_games_for_this_number_of_players(10)
-for _ in list_games_for_needed_player:
-    print(_)
+for element in list_games_for_needed_player:
+    print(element)
